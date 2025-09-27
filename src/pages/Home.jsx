8c71f2c0bb28c5a4ea75ba296467a7353c011a7c
Julieta <julysayago12@gmail.com>
2025-09-27 10:51:52 -0300

    Agrego archivos del proyecto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import "../styles/Home.css";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+}
+
+export default Home;
