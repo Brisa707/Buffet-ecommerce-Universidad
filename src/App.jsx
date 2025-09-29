a713c4aaef704126f26756fa13e2887d0ded4dfc
Rocio Maldonado <138414271+RocioMaldonado@users.noreply.github.com>
2025-09-28 21:24:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Contacto from "./pages/Contacto";
+import Productos from "./pages/Productos";
+import ProductoDetalle from "./pages/ProductoDetalle";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/contacto" element={<Contacto />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/producto/:id" element={<ProductoDetalle />} />
       </Routes>
     </BrowserRouter>
   );
