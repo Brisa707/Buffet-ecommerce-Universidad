c239dfae5494bd41732951d93bbc2534d34683bb
Rocio Maldonado <138414271+RocioMaldonado@users.noreply.github.com>
2025-10-01 14:13:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Contacto from "./pages/Contacto";
 import Productos from "./pages/Productos";
-import ProductoDetalle from "./pages/ProductoDetalle";
+import ProductoDetalle from "./pages/ProductoDetalle"; 
+import Carrito from "./pages/Carrito";
+import Pedidos from "./pages/Pedidos";
+import Detalle from "./pages/Detalle";
+import QRPage from "./pages/QRPage";
+import Perfil from "./pages/Perfil";
 
 function App() {
   return (
@@ -16,6 +21,11 @@ function App() {
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/productos" element={<Productos />} />
         <Route path="/producto/:id" element={<ProductoDetalle />} />
+        <Route path="/carrito" element={<Carrito />} />
+        <Route path="/pedidos" element={<Pedidos />} />
+        <Route path="/detalle/:id" element={<Detalle />} />
+        <Route path="/qr/:id" element={<QRPage />} />
+        <Route path="/perfil" element={<Perfil />} />
       </Routes>
     </BrowserRouter>
   );
